4f197c509af6c3d03b6b00c1211017111a9961e0
AswinVishnuA <50040037+AswinVishnuA@users.noreply.github.com>
2021-06-20 12:44:43 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,29 +1,27 @@
 
+CREATE TABLE publisher (id INTEGER PRIMARY KEY,
+			 name TEXT,
+			 country TEXT
+			 );			 ;
 
-CREATE TABLE publisher(
-    id integer PRIMARY KEY,
-    name TEXT,country TEXT 
-);
-
-CREATE TABLE books(
-    id integer PRIMARY KEY,
-    title TEXT,
-    publisher integer,
-    CONSTRAINT fk_publisher
-    FOREIGN KEY(publisher) REFERENCES publisher(id)
-);
-
-CREATE TABLE subjects(
-    id integer PRIMARY KEY,
-    name TEXT
-);
-
-CREATE TABLE books_subjects(
-    book integer,
-    subject integer,
-    CONSTRAINT fk_books 
-    foreign key(book) references books(id),
-    CONSTRAINT fk_subjects
-    foreign key(subject) references subjects(id)
-);
-
+CREATE TABLE books (id INTEGER PRIMARY KEY,
+		     title TEXT,
+		     publisher INTEGER,
+		     CONSTRAINT fk_publisher
+		     	FOREIGN KEY (publisher)
+		     	REFERENCES publisher (id)
+		     );			
+		     
+CREATE TABLE subjects (id INTEGER PRIMARY KEY,
+			name TEXT
+			);
+			
+CREATE TABLE books_subjects (book INTEGER,
+			      subject INTEGER,
+			      CONSTRAINT fk_books
+			      	FOREIGN KEY (book)
+			      	REFERENCES books(id),
+			      CONSTRAINT fk_subjects
+			      	FOREIGN KEY (subject)
+			      	REFERENCES subjects(id)
+			      	);
